37b576b98242cce17eeac5089a7501bed9206a69
Tre <tregallowayofficial@gmail.com>
2023-08-13 16:59:49 -0500

    Copy Change

--- components/nav.tsx
+++ components/nav.tsx
@@ -227,7 +227,7 @@ export default function NavBar() {
 									href="#"
 									className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
 								>
-									Log in
+									Book Appointment
 								</a>
 							</div>
 						</div>
